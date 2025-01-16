76680bea23767adbe0b25cd980a4947b89bd339e
Anton <zanton133@gmail.com>
2025-01-16 15:39:21 +0000

    Добавили рубокоп

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# Основной хелпер
 module ApplicationHelper
 end
--- review.rb
+++ review.rb
@@ -2,8 +2,8 @@
 
 source 'https://rubygs.g'
 
-git_source(:githu |repo| "https://github.#{repo}.git" }
+git_source(:githu | repo | "https://github.#{repo}.git")
 
 ruby '3.3.2'
 
-gem 'rails''~7.1.3'
+gem 'rails~7.1.3'
